3badc36a1915b55ec1f6d751ef8fb5187fdfc8c5
phawitb <96680592+phawitb@users.noreply.github.com>
2023-09-27 14:54:02 +0700

    Add files via upload

--- Profile.py
+++ Profile.py
@@ -4,19 +4,17 @@ from hash import hash_password,verify_password,load_users,save_users,isEmail
 import datetime
 from sent_email import sent_otp
 import random
-# from streamlit.source_util import (
-#     page_icon_and_name, 
-#     calc_md5, 
-#     get_pages,
-#     _on_pages_changed
-# )
-from pathlib import Path
-
 from streamlit_js_eval import streamlit_js_eval
-
 from st_pages import Page, show_pages, add_page_title
 
-# Optional -- adds the title and icon to the current page
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 add_page_title()
 show_pages(
     [
@@ -25,17 +23,8 @@ show_pages(
         Page("pages/2_Favorate.py", "Favorate", ":star:"),
         Page("pages/3_About.py", "About", ":books:"),
         Page("pages/favorateApi.py", "-",""),
-
-
-
-        
-
-
-        
     ]
 )
-
-
 
 screen_width = streamlit_js_eval(js_expressions='screen.width', key = 'SCR')
 
@@ -47,72 +36,8 @@ try:
 except:
     pass
 
-
-
-# st.write(f"Screen width is {streamlit_js_eval(js_expressions='screen.width', key = 'SCR')}")
-# st.write(f"Screen height is {streamlit_js_eval(js_expressions='screen.height', key = 'SCR1')}")
-
-
-# # hide show pages https://discuss.streamlit.io/t/hide-show-pages-in-multipage-app-based-on-conditions/28642/6
-# def add_page(main_script_path_str, page_name):
-    
-#     pages = get_pages(main_script_path_str)
-#     main_script_path = Path(main_script_path_str)
-#     pages_dir = main_script_path.parent / "pages"
-#     script_path = [f for f in pages_dir.glob("*.py") if f.name.find(page_name) != -1][0]
-#     script_path_str = str(script_path.resolve())
-#     pi, pn = page_icon_and_name(script_path)
-#     psh = calc_md5(script_path_str)
-#     pages[psh] = {
-#         "page_script_hash": psh,
-#         "page_name": pn,
-#         "icon": pi,
-#         "script_path": script_path_str,
-#     }
-#     _on_pages_changed.send()
-
-# add_page("pages", "favorateApi")
-
-
-# def delete_page(main_script_path_str, page_name):
-
-#     current_pages = get_pages(main_script_path_str)
-#     st.write(current_pages)
-
-#     for key, value in current_pages.items():
-#         if value['page_name'] == page_name:
-#             del current_pages[key]
-#             break
-#         else:
-#             pass
-#     _on_pages_changed.send()
-# current_pages = get_pages('pages')
-# # st.write(current_pages)
-# # if st.button("delete"):
-# delete_page("pages", "favorateApi")
-
-
-
-# st.set_page_config(
-#     page_title="Ex-stream-ly Cool App",
-#     page_icon="🧊",
-#     layout="wide",
-#     initial_sidebar_state="expanded",
-#     menu_items={
-#         'Get Help': 'https://www.extremelycoolapp.com/help',
-#         'Report a bug': "https://www.extremelycoolapp.com/bug",
-#         'About': "# This is a header. This is an *extremely* cool app!"
-#     }
-# )
-
-
-# st.title("Login")
-# tab2.subheader("A tab with the data")
-
 cookie_manager = stx.CookieManager()
 person_id = cookie_manager.get(cookie='person_id')
-
-# st.write(person_id)
 
 if not person_id:
     LIST_MENUS = ["📈 Login", "🗃 Register","Forgot Password"]
@@ -157,7 +82,6 @@ if not person_id:
             tab2.text('email already register')
 
         elif pass1 == pass2 and pass1:
-            # st.text('password match')
             if tab2.button("sent otp"):
                 tab2.text('sent otp...')
 
@@ -177,10 +101,8 @@ if not person_id:
         otp = tab2.text_input("OTP :")
         if tab2.button("comfirm OTP"):
             if st.session_state["randomotp"] == otp:
-                # st.text('OTP match')
-                
+               
                 users = load_users("users.json")
-
                 username = st.session_state["email"]
                 password = st.session_state["password"]
                 hashed_password = hash_password(password)
@@ -194,7 +116,6 @@ if not person_id:
                 tab2.text('OTP unmatch')
 
     #forgot password
-
     if "randomotp" not in st.session_state:
         st.session_state["randomotp"] = False
     if "stage_fg" not in st.session_state:
@@ -249,12 +170,6 @@ if not person_id:
 
             else:
                 tab3.text('New password and Re-New password must same value')
-
-
-
-
-
-
     
 else:
     LIST_MENUS = ["📈 My Profile", "🗃 Change Password"]
@@ -272,16 +187,11 @@ else:
             pass
 
     #change password
-
     tab2.text(person_id)
 
     login_password = tab2.text_input("Old Password :",type="password")
     new_passsword = tab2.text_input("New Password :", type="password")
     renew_passsword = tab2.text_input("Re-New Password :", type="password")
-
-    # st.text(login_password)
-    # st.text(new_passsword)
-
     users = load_users("users.json")
 
     if login_password and verify_password(users[person_id], login_password):
@@ -294,7 +204,6 @@ else:
             tab2.text("New Password must same Re-New Password!!")
         if tab2.button('CONFIRM'):
             if MATCH:
-                
                 username = person_id
                 password = new_passsword
                 hashed_password = hash_password(password)

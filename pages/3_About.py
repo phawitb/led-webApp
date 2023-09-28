205dd7b79b0a05bff62147dc7a35215257006da4
phawitb <96680592+phawitb@users.noreply.github.com>
2023-09-28 08:06:11 +0700

    Update 3_About.py

--- pages/3_About.py
+++ pages/3_About.py
@@ -3,18 +3,18 @@ import pandas as pd
 import requests
 import io
 
-st.markdown(
-    """
-    <style>
-    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
-    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
-    .viewerBadge_text__1JaDK {
-        display: none;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# st.markdown(
+#     """
+#     <style>
+#     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+#     .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+#     .viewerBadge_text__1JaDK {
+#         display: none;
+#     }
+#     </style>
+#     """,
+#     unsafe_allow_html=True
+# )
 
 st.markdown('### About')
 
